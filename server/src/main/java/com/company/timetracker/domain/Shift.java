a2bf94c9e0d160149d137895d9b1561cd3e249be
Nico Döbele <nico.doebele@googlemail.com>
2023-05-18 23:26:28 +0200

    implemented shift entity

---
+++ server/src/main/java/com/company/timetracker/domain/Shift.java
@@ -0,0 +1,65 @@
+package com.company.timetracker.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+@Entity
+public class Shift {
+
+    @Id
+    @GeneratedValue
+    @JsonProperty(access = Access.READ_ONLY)
+    private long id;
+
+    @ManyToOne
+    private Person person;
+
+    private Date startDate;
+    
+    private Date endDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Shift [id=" + id + ", person=" + person + ", startDate=" + startDate + ", endDate=" + endDate + "]";
+    }
+}
