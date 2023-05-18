be7b99d06ec362426478c64975cf512c4c5cd0c0
Nico Döbele <nico.doebele@googlemail.com>
2023-05-18 23:07:07 +0200

    initial commit

---
+++ server/src/main/java/com/company/timetracker/TimeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.company.timetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrackerApplication.class, args);
+	}
+
+}
